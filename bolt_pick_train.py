62edabe360503832bb61ee43909aa465aa8c4eff
abeobk <abeobk@gmail.com>
2024-08-23 14:10:38 +0900

    updated

--- bolt_pick_train.py
+++ bolt_pick_train.py
@@ -4,5 +4,5 @@ import cv2
 
 model = YOLO("yolov8s-seg.pt") # load pretrain model for training
 # model = YOLO("yolov8l-seg.pt") # load pretrain model for training
-results = model.train(data="./datasets/bolt3/dataset.yaml",epochs=100,imgsz=640,device='cuda',
+results = model.train(data="./datasets/train.yaml",epochs=100,imgsz=640,device='cuda',
                       augment=True, scale=0.1, translate = 0.1,degrees=30)
