9bd1f48a3dc8b94e62364c4c684ec527a17d5d27
abeobk <abeobk@gmail.com>
2024-08-26 18:54:10 +0900

    asfd

--- bolt_pick_train.py
+++ bolt_pick_train.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import numpy as np
 import cv2
 
-model = YOLO("yolov8s-seg.pt") # load pretrain model for training
+model = YOLO("best.pt") # load pretrain model for training
 # model = YOLO("yolov8l-seg.pt") # load pretrain model for training
 results = model.train(data="./dataset.yaml",epochs=100,imgsz=640,device='cuda',
                       augment=True, scale=0.1, translate = 0.1,degrees=30)
